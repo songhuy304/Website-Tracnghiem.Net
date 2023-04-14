bc5caac82c282861f802569a4e5c432236eededb
songhuy304 <songhuy304@GMAIL.com>
2023-04-14 19:02:45 +0700

    Helo

--- DoAnCs/Areas/Admin/Controllers/QuestionController.cs
+++ DoAnCs/Areas/Admin/Controllers/QuestionController.cs
@@ -8,6 +8,7 @@ using System.Web;
 using System.Web.Configuration;
 using System.Web.Mvc;
 using DoAnCs.Models;
+using PagedList;
 
 namespace DoAnCs.Areas.Admin.Controllers
 {
@@ -16,43 +17,55 @@ namespace DoAnCs.Areas.Admin.Controllers
         private TracNghiemEntities1 db = new TracNghiemEntities1();
 
         // GET: Admin/Question
-        public ActionResult Index()
+        public ActionResult Index(string currentFilter, int? page, string searchString)
         {
-    
-            var questions = db.Questions.OrderByDescending(x=>x.IdSubject);
-            return View(questions.ToList());
-        }
+            var item = new List<Question>();
 
-        // GET: Admin/Question/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Question question = db.Questions.Find(id);
-            if (question == null)
-            {
-                return HttpNotFound();
-            }
-            return View(question);
-        }
 
-        // GET: Admin/Question/Create
+            if (searchString != null) /*nếu ô tìm kiếm bằng khác null  thì bắt đầu từ  page  1*/
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                //lấy ds sản phẩm theo từ khóa
+                item = db.Questions.Where(x => x.Contentt.Contains(searchString) || searchString == null).ToList();
+
+            }
+            else
+            {
+             
+                item = db.Questions.ToList();
+            }
+            ViewBag.CurrentFilter = searchString;
+            int pageIndex = (page ?? 1);
+            int pagesize = 3; /*số lượng item của trang = 5*/
+            item = item.OrderByDescending(n => n.IdSubject).ToList();
+            ViewBag.PageSize = pagesize;
+            ViewBag.Page = page;
+            return View(item.ToPagedList(pageIndex, pagesize));
+
+
+
+        }
         public ActionResult Create()
         {
             ViewBag.IdDifficulty = new SelectList(db.Difficulties, "IdDifficulty", "NameDifficulty");
-            
+
             ViewBag.Ans = new SelectList(db.Answers.ToList(), "IdAnswer", "DapAn");
 
             ViewBag.IdSubject = new SelectList(db.Subjects, "IdSubject", "NameSubject");
             return View();
         }
 
-       
+
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create( Question question)
+        public ActionResult Create(Question question)
         {
             if (ModelState.IsValid)
             {
@@ -69,75 +82,56 @@ namespace DoAnCs.Areas.Admin.Controllers
         }
 
         // GET: Admin/Question/Edit/5
-        public ActionResult Edit(int? id)
+
+        [HttpGet]
+        public ActionResult edit(int id)
         {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Question question = db.Questions.Find(id);
-            if (question == null)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.IdDifficulty = new SelectList(db.Difficulties, "IdDifficulty", "NameDifficulty", question.IdDifficulty);
-            ViewBag.IdSubject = new SelectList(db.Subjects, "IdSubject", "NameSubject", question.IdSubject);
+            ViewBag.IdDifficulty = new SelectList(db.Difficulties, "IdDifficulty", "NameDifficulty");
+
+            ViewBag.Ans = new SelectList(db.Answers.ToList(), "IdAnswer", "DapAn");
+
+            ViewBag.IdSubject = new SelectList(db.Subjects, "IdSubject", "NameSubject");
+            var question = db.Questions.Find(id);
             return View(question);
         }
-
-        // POST: Admin/Question/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "IdQuestion,Contentt,optionA,optionB,optionC,optionD,IdSubject,IdDifficulty")] Question question)
+        public ActionResult edit(Question question)
+
         {
+
+
             if (ModelState.IsValid)
             {
-                db.Entry(question).State = EntityState.Modified;
+                db.Questions.Attach(question);
+                db.Entry(question).State = System.Data.Entity.EntityState.Modified;
+
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
-            ViewBag.IdDifficulty = new SelectList(db.Difficulties, "IdDifficulty", "NameDifficulty", question.IdDifficulty);
-            ViewBag.Ans = new SelectList(db.Answers, "IdAnswer", "DapAn");
 
-            ViewBag.IdSubject = new SelectList(db.Subjects, "IdSubject", "NameSubject", question.IdSubject);
             return View(question);
         }
+        //Xóa Không load trang khác  
 
-        // GET: Admin/Question/Delete/5
         public ActionResult Delete(int? id)
         {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Question question = db.Questions.Find(id);
-            if (question == null)
-            {
-                return HttpNotFound();
-            }
-            return View(question);
-        }
 
-        // POST: Admin/Question/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Question question = db.Questions.Find(id);
+            var question = db.Questions.Find(id);
+
             db.Questions.Remove(question);
             db.SaveChanges();
-            return RedirectToAction("Index");
-        }
+            return RedirectToAction("Index", "Question");
 
-        protected override void Dispose(bool disposing)
+        }
+        public ActionResult detail(int? id)
         {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
+            var question = db.Questions.SingleOrDefault(m => m.IdQuestion == id);
+            ViewBag.Id = question.IdQuestion;
+
+            return View(question);
         }
     }
+
+
 }
+
